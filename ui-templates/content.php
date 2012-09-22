46ebb6e6c43c04d13649e35b83f8f2284227c7ea
Azrul M. Amir <azrul@azrul.net>
2012-09-22 15:07:05 +0800

    Removed Unnecessary Files
    Class/modules is no longer/yet required.

--- ui-templates/content.php
+++
@@ -1,7 +0,0 @@
-<div id="content-head-container">
-	The Title
-</div>
-
-<div id="content-container">
-	The Content
-</div>
--- ui-templates/sidebar.php
+++
@@ -1,3 +0,0 @@
-<div id="sidebar-container" class="span2">
-	<p>This is the sidebar</p>
-</div>
